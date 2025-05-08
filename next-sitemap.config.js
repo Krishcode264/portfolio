3091ad90cb10ef6dfa117749edb7365d497a137e
Krishcode264 <krishnazade99@gmail.com>
2025-05-08 09:00:24 +0530

    push it again

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://krishnazade.vercel.app',
-    generateRobotsTxt: true,
-  };
-  
+  siteUrl: 'https://krishnazade.vercel.app',
+  generateRobotsTxt: true,
+  transform: async (config, path) => {
+    return {
+      loc: `${config.siteUrl}${path}`,
+      changefreq: 'daily',
+      priority: path === '/' ? 1.0 : 0.7, // Example to set 1.0 priority for homepage
+      lastmod: new Date().toISOString(),
+    };
+  },
+};
